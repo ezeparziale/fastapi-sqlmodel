0ceb80b6a7157dd737dd45ca06973d91370bcdbf
Ezequiel Parziale <eparziale@gmail.com>
2022-12-08 20:13:29 -0300

    :rocket: update: add database file

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlmodel import Session, create_engine
+
+from .config import settings
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+def get_session():
+    with Session(autocommit=False, autoflush=False, bind=engine) as session:
+        yield session
