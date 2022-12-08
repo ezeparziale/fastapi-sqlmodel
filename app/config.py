25b60280a9ab4a4462df76b2a164f9341d11c3d6
Ezequiel Parziale <eparziale@gmail.com>
2022-12-08 20:13:15 -0300

    :rocket: update: add config file

---
+++ app/config.py
@@ -0,0 +1,37 @@
+from typing import Any, Dict, Optional
+
+from pydantic import BaseSettings, PostgresDsn, validator
+
+
+class Settings(BaseSettings):
+    # FastAPI
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "FastAPI + SQLModel"
+
+    # Database
+    POSTGRES_HOSTNAME: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOSTNAME"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
