f9fea1c8b0ff69427c348222cb72d9f84838a300
Ezequiel Parziale <eparziale@gmail.com>
2022-12-08 23:37:18 -0300

    :wrench: update: use uri sql var

--- app/db/database.py
+++ app/db/database.py
@@ -4,8 +4,7 @@ from app.core.config import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
 
 def get_session():
     with Session(autocommit=False, autoflush=False, bind=engine) as session:
