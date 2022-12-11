1a1f5d641d7b7402bd413076b5a6bbba5eec5aed
Ezequiel Parziale <eparziale@gmail.com>
2022-12-10 21:21:00 -0300

    :fire: update: remove db creation tables

--- app/main.py
+++ app/main.py
@@ -22,12 +22,6 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
-from app.db.database import engine
-from app.models.models import SQLModel
-
-SQLModel.metadata.create_all(engine)
-
-
 # Routes
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
