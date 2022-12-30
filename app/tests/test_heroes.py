e50b2924a3e50606e94db9d49b56f53100876b81
Ezequiel Parziale <eparziale@gmail.com>
2022-12-30 20:02:45 -0300

    :rotating_light: update: add nosec for bandit ignore

--- app/tests/test_heroes.py
+++ app/tests/test_heroes.py
@@ -25,7 +25,7 @@ def test_read_heroes(session: Session, client: TestClient, test_hero_1, test_her
 def test_create_hero(client: TestClient):
     response = client.post(
         "/api/v1/heros/",
-        json={"name": "Green Lantern", "secret_name": "Hal Jordan", "age": 50},  # nosec
+        json={"name": "Green Lantern", "secret_name": "Hal Jordan", "age": 50},  #nosec B106
     )
     data = response.json()
 
@@ -71,7 +71,7 @@ def test_update_hero(session: Session, client: TestClient):
     session.commit()
 
     response = client.patch(
-        f"/api/v1/heros/{hero_1.id}", json={"secret_name": "Barry Allen"}  # nosec
+        f"/api/v1/heros/{hero_1.id}", json={"secret_name": "Barry Allen"}  #nosec B106
     )
     data = response.json()
 
