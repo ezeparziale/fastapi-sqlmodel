15fbb65383b3a65feb8bd0855aa73d492ea38130
Ezequiel Parziale <eparziale@gmail.com>
2025-09-07 18:31:08 -0300

    :zap: refactor: improve db conn

--- app/db/database.py
+++ app/db/database.py
@@ -2,11 +2,12 @@ from sqlmodel import Session, create_engine
 
 from app.core.config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI.unicode_string())
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
+SessionLocal = Session(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_session():
+
     with Session(autocommit=False, autoflush=False, bind=engine) as session:
         yield session
