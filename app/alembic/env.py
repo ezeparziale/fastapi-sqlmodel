89ddbf3673a0c979c3f550b86850b6b1f4ced4de
Ezequiel Parziale <eparziale@gmail.com>
2025-09-07 18:29:32 -0300

    :zap: refactor(alembic): improve alembic files
    - move alembic to app folder
    - update alembic.ini
    - add new revision

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,18 +1,22 @@
 from logging.config import fileConfig
 
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+from sqlmodel import SQLModel
+
 from app.core.config import settings
 
 # SQLModel import
 from app.models import *  # noqa: F403, F401
-from sqlalchemy import engine_from_config, pool
-from sqlmodel import SQLModel
-
-from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_DATABASE_URI)  # type: ignore
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.SQLALCHEMY_DATABASE_URI.unicode_string(),
+)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -62,7 +66,7 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
